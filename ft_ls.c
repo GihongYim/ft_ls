328a519a16d06d6170b0cbe569da252b40aba972
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-28 21:14:24 +0900

    add opendir

--- ft_ls.c
+++ ft_ls.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <dirent.h>
 #include "libft.h"
 
 int main(int argc, char* argv[])
 {
-    
+    char *target = ".";
+    if (argc > 3)
+        return 0;
+    if (argc == 2) {
+        target = argv[1];
+    }
+    DIR * dir = opendir(target);
+    struct dirent *dd;
+    while (1) {
+        dd = readdir(dir);
+        if (dd == NULL)
+            break;
+        printf("%s\n", dd->d_name);
+    }
     return 0;
 }
