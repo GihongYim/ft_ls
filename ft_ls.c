04ff82aa793a146af1ecef62518adac92651f413
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-30 21:04:40 +0900

    asdf

--- ft_ls.c
+++ ft_ls.c
@@ -24,38 +24,71 @@ enum sort_type
 static int recursive = false;
 static int allOption = false;
 
+int getFiles(struct dirent** files, char *path) {
+    int len = 0;
+    struct dirent * dd;
+    int index = 0;
+
+    DIR *dir = opendir(path);
+
+    while (1) {
+        dd = readdir(dir);
+        if (dd == NULL) break;
+        len++;
+    }
+    closedir(dir);
+    *files = malloc(sizeof(struct dirent) * len);
+
+    dir = opendir(path);
+    for (int i = 0; i < len; i++) {
+        dd = readdir(dir);
+        if (dd == NULL) break;
+        files[index++] = dd;
+    }
+    closedir(dir);
+    return len;
+}
+
 void printDir(char *path, enum format format, enum sort_type sort_type) 
 {
-    DIR *dir = opendir(path);
-    struct dirent *curr;
-    char *next = NULL;
+    // DIR *dir = opendir(path);
+    // struct dirent *curr;
+    // char *next = NULL;
+    int numOfFile;
+    struct dirent *files = NULL;
+
+    numOfFile = getFiles(&files, path);
+    printf("numOfFile: %d\n", numOfFile);
+    for (int i = 0; i < numOfFile; i++) {
+        printf("%d: %s\n", i, files[i].d_name);
+    }
 
     sort_type = sort_name;
     format = only_file_name;
-    while (1) {
-        curr = readdir(dir);
-        if (curr == NULL)
-            break;
-        if (!allOption && curr->d_name[0] == '.') continue;
-        if (recursive && curr->d_type == DT_DIR) continue;
-        printf("%s ", curr->d_name);
-    }
-    closedir(dir);
-    if (!recursive) return;
-    dir = opendir(path);
-    while (1) {
-        curr = readdir(dir);
-        if (curr == NULL)
-            break;
-        if (!allOption && curr->d_name[0] == '.') continue;
-        if (curr->d_type == DT_DIR)  {
-            next = ft_strjoin(path, "/");
-            next = ft_strjoin(next, curr->d_name);
-            printf("\n");
-            printDir(next, format, sort_type);
-        }
-    }
-    closedir(dir);
+    // while (1) {
+    //     curr = readdir(dir);
+    //     if (curr == NULL)
+    //         break;
+    //     if (!allOption && curr->d_name[0] == '.') continue;
+    //     if (recursive && curr->d_type == DT_DIR) continue;
+    //     printf("%s ", curr->d_name);
+    // }
+    // closedir(dir);
+    // if (!recursive) return;
+    // dir = opendir(path);
+    // while (1) {
+    //     curr = readdir(dir);
+    //     if (curr == NULL)
+    //         break;
+    //     if (!allOption && curr->d_name[0] == '.') continue;
+    //     if (curr->d_type == DT_DIR)  {
+    //         next = ft_strjoin(path, "/");
+    //         next = ft_strjoin(next, curr->d_name);
+    //         printf("\n");
+    //         printDir(next, format, sort_type);
+    //     }
+    // }
+    // closedir(dir);
 }
 
 
@@ -90,4 +123,5 @@ int main(int argc, char *argv[])
     }
     printDir(".", format, sort_type);
     printf("\n");
+    return 0;
 }
--- fun_test/opendir.c
+++ fun_test/opendir.c
@@ -2,6 +2,21 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+
+int dirSize(char *dir) {
+	DIR *dd = opendir(dir);
+	int len = 0;
+	struct dirent *dp;
+
+	while (1) {
+		dp = readdir(dd);
+		if (dp == NULL)	break;
+		len++;
+	}
+	printf("%d\n", len);
+	return len;
+}
+
 int main(void)
 {
 	DIR	*dd = opendir("..");
@@ -11,17 +26,23 @@ int main(void)
 		return 1;
 	}
 
-	struct dirent *dp;
+	// printf("__dd_buf: %s\n", dd->__dd_buf);
+	// printf("__dd_len: %d\n", dd->__dd_len);
+	// printf("__dd_loc: %ld\n", dd->__dd_loc);
+	// printf("__dd_size: %ld\n", dd->__dd_size);
+	int len = dirSize("..");
+	// struct dirent *dp;
 
-	while (dp = readdir(dd)) {
-		printf("d_name: %s\n", dp->d_name);
-		printf("d_ino: %d\n", dp->d_ino);
-		// printf("d_off: %d\n", dp->d_off);
-		printf("d_reclen: %ud\n", dp->d_reclen);
-		printf("d_type: %uc\n", dp->d_type);
-		printf("sizeof(dp): %d\n", sizeof(dp));
-		putchar('\n');
-	}
+	// while (dp = readdir(dd)) {
+	// 	printf("d_name: %s\n", dp->d_name);
+	// 	printf("d_ino: %d\n", dp->d_ino);
+	// 	// printf("d_off: %d\n", dp->d_off);
+	// 	printf("d_reclen: %ud\n", dp->d_reclen);
+	// 	printf("d_type: %uc\n", dp->d_type);
+	// 	printf("sizeof(dp): %d\n", sizeof(dp));
+	// 	putchar('\n');
+	// }
+	closedir(dd);
 
 	return 0;
 }
